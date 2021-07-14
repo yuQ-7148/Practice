77cbc3feb75acba1d7a2c4f76cb8fa1ea6fe52c5
yuQ-7148 <1159915745@qq.com>
2021-07-14 15:29:41 +0800

    创建组件类

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+ 
+var h1 = React.createElement('h1', null, 'Hello World!')
+class HelloWorld extends React.Component{
+  render() {
+    return React.createElement('div', null, h1, h1)
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  React.createElement(HelloWorld, null),
+  document.getElementById('content')
+)
