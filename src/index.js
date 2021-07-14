f33a37d8e4b8d56996597a0dc051f74f8b9410c7
yuQ-7148 <1159915745@qq.com>
2021-07-14 15:37:26 +0800

    创建组件类（补充）

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
  
 var h1 = React.createElement('h1', null, 'Hello World!')
-class HelloWorld extends React.Component{
+class HelloWorld extends React.Component{   
+  //包含React组件的变量的名称应该首字母大写
   render() {
     return React.createElement('div', null, h1, h1)
   }
