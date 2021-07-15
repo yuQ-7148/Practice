1f4a0944b0ac90e54ec40041b0a206b4ade46ce2
yuQ-7148 <1159915745@qq.com>
2021-07-15 09:24:28 +0800

    组件中使用JSX 输出变量 属性

--- src/index.js
+++ src/index.js
@@ -7,39 +7,32 @@ import './index.css';
 class HelloWorld extends React.Component{   
   //包含React组件的变量的名称应该首字母大写
   render() {
-    /*
-    console.log(Object.isFrozen(this.props))
-  
-    if (this.props.heading) return <h1>Hello</h1>
-    else return <p>Hello</p>*/
-    //createClass()中的render()方法只能返回单个元素
-
-    return React.createElement('h1', null, 'Hello ' + this.props.frameworkName + ' world!!!')
-    //HelloWorld组件所有属性被传递到<h1>元素中
+    return (
+      <h1>Hello {this.props.frameworkName} world!!!</h1>
+      //用花括号输出变量
+    )
   }
 }
 
 ReactDOM.render(
   //React.createElement(HelloWorld, null),
-  React.createElement(
-    'div',
-    null,
-    React.createElement(HelloWorld, {
-      id: 'ember',
-      frameworkName: 'Ember.js',
-      title: 'A framework for creating ambitious web applications'
-    }),
-    React.createElement(HelloWorld, {
-      id: 'backbone',
-      frameworkName: 'Backbone.js',
-      title: 'Backbone.js gives structure to web applications...'
-    }),
-    React.createElement(HelloWorld, {
-      id: 'angular',
-      frameworkName: 'Angular.js',
-      title: 'Superheroic JavaScript MVM Framework'
-    })
-  ),
+  <div>
+    <HelloWorld
+      id="ember"
+      frameworkName="Ember.js"
+      title="A framework for creating ambitious web applications"
+    ></HelloWorld>
+    <HelloWorld
+      id="backbone"
+      frameworkName="Backbone.js"
+      title="Backbone.js gives structure to web applications..."
+    ></HelloWorld>
+    <HelloWorld
+      id="angular"
+      frameworkName="Angular.js"
+      title="Superheroic JavaScript MVW Framework"
+    ></HelloWorld>
+  </div>,
   //创建组件时，父组件为子组件分配属性。子组件不应该修改它的属性
   document.getElementById('content')
 )
