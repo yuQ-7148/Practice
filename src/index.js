9adb4ba145d6449c6f9df4b47663389d620da899
yuQ-7148 <1159915745@qq.com>
2021-07-15 09:58:16 +0800

    React组件的创建和调用

--- src/index.js
+++ src/index.js
@@ -5,34 +5,23 @@ import './index.css';
 //var h1 = React.createElement('h1', null, 'Hello World!')
 
 class HelloWorld extends React.Component{   
-  //包含React组件的变量的名称应该首字母大写
+  getUrl() {
+    return 'http://webapplog.com'
+  }
   render() {
     return (
-      <h1>Hello {this.props.frameworkName} world!!!</h1>
-      //用花括号输出变量
+      <div>
+        <p>Your REST API URL is:
+          <a href={this.getUrl()}>
+            {this.getUrl()}
+          </a>
+        </p>
+      </div>
     )
   }
 }
 
 ReactDOM.render(
-  //React.createElement(HelloWorld, null),
-  <div>
-    <HelloWorld
-      id="ember"
-      frameworkName="Ember.js"
-      title="A framework for creating ambitious web applications"
-    ></HelloWorld>
-    <HelloWorld
-      id="backbone"
-      frameworkName="Backbone.js"
-      title="Backbone.js gives structure to web applications..."
-    ></HelloWorld>
-    <HelloWorld
-      id="angular"
-      frameworkName="Angular.js"
-      title="Superheroic JavaScript MVW Framework"
-    ></HelloWorld>
-  </div>,
-  //创建组件时，父组件为子组件分配属性。子组件不应该修改它的属性
+  <HelloWorld></HelloWorld>,
   document.getElementById('content')
 )
