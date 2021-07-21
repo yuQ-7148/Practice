62ef40d589c3c70a27a747f2c83ed68b4c7432d7
yuQ-7148 <1159915745@qq.com>
2021-07-21 15:41:21 +0800

    setState用法

--- src/index.js
+++ src/index.js
@@ -2,15 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
  
-async function fetchText() {
-  let response = await fetch('/readme.txt');
-  if (response.status >= 200 && response.status < 300) {
-    return await response.text();
-  } else {
-    throw new Error(response.statusText);
+function plusone(cc, props){
+  console.log(cc);
+  return{count: cc.count + 1};
+}
+
+class AddByFunction extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {count: 0};
+      this.increment = this.increment.bind(this);
+  }
+
+  increment() {
+      this.setState(plusone);
+      this.setState(plusone);
+
+  }
+
+  render(){
+      console.log('cc');
+      return (
+          <div>
+              <button onClick={this.increment}>AddByFunction</button>
+              <h2>Result:{this.state.count}</h2>
+          </div>
+      );
   }
 }
 
 ReactDOM.render(
-  document.getElementById('content')
-)
+  <AddByFunction />,
+  document.getElementById('root')
+);
